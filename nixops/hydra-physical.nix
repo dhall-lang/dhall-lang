336141163f205f3c2cde6c76f36d9284445f406b
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-01-15 22:12:33 -0800

    Fix `NIX_PATH` failure

--- nixops/hydra-physical.nix
+++ nixops/hydra-physical.nix
@@ -1,34 +1,43 @@
-{ ... }: {
-  imports = [ <nixpkgs/nixos/modules/profiles/qemu-guest.nix> ];
+let
+  hydraNixpkgs =
+    builtins.fetchTarball {
+      url = "https://github.com/NixOS/nixpkgs/archive/2437bb394392322d28d4244a63ab9b7ae8cc18dd.tar.gz";
 
-  nixpkgs.system = "x86_64-linux";
+      sha256 = "1c26n0z2zsapc1hxww1rpj056f8ah76f11h4f6wqjzjlj40i8jrq";
+    };
 
-  boot = {
-    initrd.availableKernelModules =
-      [ "ata_piix" "virtio_pci" "floppy" "sd_mod" ];
+in
+  { ... }: {
+    imports = [ "${hydraNixpkgs}/nixos/modules/profiles/qemu-guest.nix" ];
 
-    kernelParams = [ "console=ttyS0,19200n8" ];
+    nixpkgs.system = "x86_64-linux";
 
-    loader = {
-      grub = {
-        enable = true;
+    boot = {
+      initrd.availableKernelModules =
+        [ "ata_piix" "virtio_pci" "floppy" "sd_mod" ];
 
-        extraConfig = ''
-          serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
-          terminal_input serial;
-          terminal_output serial;
-        '';
+      kernelParams = [ "console=ttyS0,19200n8" ];
 
-        device = "nodev";
+      loader = {
+        grub = {
+          enable = true;
 
-        timeout = 10;
+          extraConfig = ''
+            serial --speed=19200 --unit=0 --word=8 --parity=no --stop=1;
+            terminal_input serial;
+            terminal_output serial;
+          '';
 
-        version = 2;
+          device = "nodev";
+
+          timeout = 10;
+
+          version = 2;
+        };
       };
     };
-  };
 
-  fileSystems."/" = { device = "/dev/sda"; fsType = "ext4"; };
+    fileSystems."/" = { device = "/dev/sda"; fsType = "ext4"; };
 
-  swapDevices = [ { device = "/dev/sdb"; } ];
-}
+    swapDevices = [ { device = "/dev/sdb"; } ];
+  }
