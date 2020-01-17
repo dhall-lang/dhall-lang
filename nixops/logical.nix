7f7b7e94afe8a2f191f42d5c7c23c345ca4bc8cb
Gabriel Gonzalez <Gabriel439@gmail.com>
2020-01-16 17:13:26 -0800

    Add `dhall-lang.org` as known host

--- nixops/logical.nix
+++ nixops/logical.nix
@@ -102,6 +102,11 @@
 
       publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==";
     }
+
+    { hostNames = [ "dhall-lang.org" ]
+
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBp/WR0q2LUjpzHDwm03CijnpUyvHS9CDnJYvR0YNBpT";
+    }
   ];
 
   security.sudo.wheelNeedsPassword = false;
