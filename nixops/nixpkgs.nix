1b762c9ce86b80807598d8eec13fdcad131f593d
Gabriel Gonzalez <Gabriel439@gmail.com>
2021-01-12 23:16:00 -0800

    Update Nixpkgs

--- nixops/nixpkgs.nix
+++ nixops/nixpkgs.nix
@@ -1,5 +1,5 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/fbd46be8a9aaca39950bbb5de1001a024757a1fa.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/a317a15694d6177fa4ae52caac69497b4fce6f66.tar.gz";
 
-  sha256 = "1n8iwx8mm0san4cf35rrkj6h3z01bhhhmfms1q4ipf0f5f5wpp44";
+  sha256 = "16b028s4jj4q44f0vlpx36aw6i8s8sgqsdyhkfs2yhckpmjbbccp";
 }
