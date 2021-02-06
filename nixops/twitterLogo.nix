823f9fd12acfd017c7c513ecfe1b074f614a8711
Nicholas Scheel <monoidmusician@gmail.com>
2021-02-05 21:09:48 -0500

    Update nix hashes, new source for twitter logo

--- nixops/twitterLogo.nix
+++ nixops/twitterLogo.nix
@@ -5,8 +5,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     name = "twitter-logos.zip";
-    url = "https://about.twitter.com/content/dam/about-twitter/company/brand-resources/en_us/Twitter-Logos.zip";
-    sha256 = "1dhqmj3krhak10yrq0zm89ld40a32ndrfnl6ligaaphf5dkff5m6";
+    url = "https://about.twitter.com/content/dam/about-twitter/en/brand-toolkit/downloads/twitter-logo-01282021.zip";
+    sha256 = "18ig7krfdqv8v77kiwig7fgz77zn692l4xky4xhlgfi3py86fn59";
   };
 
   sourceRoot = ".";
