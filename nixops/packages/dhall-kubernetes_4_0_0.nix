8520f46fe363be9a93719864044148e00e6993e5
Gabriella Gonzalez <Gabriel439@gmail.com>
2021-11-30 08:46:04 -0800

    Build each version of Kuberenetes API
    Related to #1242

--- nixops/packages/dhall-kubernetes_4_0_0.nix
+++ nixops/packages/dhall-kubernetes_4_0_0.nix
@@ -1,13 +1,13 @@
-{ buildDhallGitHubPackage }:
+{ buildDhallGitHubPackage, directory ? "" }:
   buildDhallGitHubPackage {
-    name = "dhall-kubernetes";
+    name = "dhall-kubernetes-${directory}";
     githubBase = "github.com";
     owner = "dhall-lang";
     repo = "dhall-kubernetes";
     rev = "v4.0.0";
     fetchSubmodules = false;
     sha256 = "0j5d9rdiwa0zajmqak1ddlmbq2lrkcsc89aa70bhlkq33d1adwyh";
-    directory = "";
+    inherit directory;
     file = "package.dhall";
     source = false;
     document = false;
