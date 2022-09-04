dhall-lang/dhall-lang

commit b5cf4dd1bdac0c572bb80f121af465b03bd7cd44

dhall-lang.org: Fix continuous deployment (#1300)
... to fix the issue reported in:

https://github.com/dhall-lang/dhall-lang/pull/1296#issuecomment-1227679350

What happened is that due to this bug in `nixos-mailserver`:

https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/issues/232

... the `switch-to-configuration` step of `self-deploy` would hang
permanently, preventing the system from picking up new changes.

However, fixing that required updating the version of `nixos-mailserver`
to the 22.05 branch, which in turn required updating the version of
`nixpkgs` to 22.05, too.

That in turn broke the `codemirror` build, so I switched to downloading
a prebuilt `codemirror` archive instead of building `codemirror` using
`node2nix`.

Also, we now no longer need to use `pkgs.hydra-unstable` now that we've
upgraded to a newer version of Nixpkgs.

* Fix `vm` build

* Fix `codemirror` build

nixops/logical.nix
@@ -15,9 +15,9 @@ in
     let
       nixos-mailserver =
         builtins.fetchTarball {
-          url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/5675b122a947b40e551438df6a623efad19fd2e7/nixos-mailserver-5675b122a947b40e551438df6a623efad19fd2e7.tar.gz";
+          url = "https://gitlab.com/simple-nixos-mailserver/nixos-mailserver/-/archive/f535d8123c4761b2ed8138f3d202ea710a334a1d/nixos-mailserver-f535d8123c4761b2ed8138f3d202ea710a334a1d.tar.gz";
 
-          sha256 = "1fwhb7a5v9c98nzhf3dyqf3a5ianqh7k50zizj8v5nmj3blxw4pi";
+          sha256 = "0csx2i8p7gbis0n5aqpm57z5f9cd8n9yabq04bg1h4mkfcf7mpl6";
         };
 
     in
@@ -107,7 +107,7 @@ in
     acme = {
       acceptTerms = true;
 
-      email = "Gabriel439@gmail.com";
+      defaults.email = "Gabriel439@gmail.com";
     };
 
     sudo.wheelNeedsPassword = false;
@@ -139,8 +139,6 @@ in
 
       notificationSender = "noreply@dhall-lang.org";
 
-      package = pkgs.hydra-unstable;
-
       useSubstitutes = true;
     };
 
@@ -151,7 +149,7 @@ in
     logrotate = {
       enable = true;
 
-      extraConfig = ''
+      configFile = ''
         /var/spool/nginx/logs/*.log {
           create 0644 nginx nginx
           daily
@@ -407,6 +405,8 @@ in
     openssh.enable = true;
   };
 
+  system.stateVersion = "21.11";
+
   systemd.services = {
     discourse =
       let
@@ -543,7 +543,9 @@ in
         build = "${repositoryDirectory}/result";
 
       in
-        { wantedBy = [ "multi-user.target" ];
+        { enable = false;
+
+          wantedBy = [ "multi-user.target" ];
 
           after = [ "network-online.target" ];
 

nixops/nixpkgs.nix
@@ -1,5 +1,5 @@
 builtins.fetchTarball {
-  url = "https://github.com/NixOS/nixpkgs/archive/369c4fe99c9eada7d73c6e878c483a3b2acc98c7.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/e5be8a487debffe650ccdbd467e26843717b021b.tar.gz";
 
-  sha256 = "1ac2nrpph5qp6dgf1hwl6f7qszh8daazr97wh3y9lbrmzya8w2nd";
+  sha256 = "1r1ywwmhkkjv088l7cd2yfilm0qs6ab8rlwa0d2x7gnrqzlmd96v";
 }

nixops/npm/default.nix
@@ -1,17 +1,0 @@
-# This file has been generated by node2nix 1.6.0. Do not edit!
-
-{pkgs ? import <nixpkgs> {
-    inherit system;
-  }, system ? builtins.currentSystem, nodejs ? pkgs."nodejs-6_x"}:
-
-let
-  nodeEnv = import ./node-env.nix {
-    inherit (pkgs) stdenv lib python2 utillinux runCommand writeTextFile;
-    inherit nodejs;
-    libtool = if pkgs.stdenv.isDarwin then pkgs.darwin.cctools else null;
-  };
-in
-import ./node-packages.nix {
-  inherit (pkgs) fetchurl fetchgit;
-  inherit nodeEnv;
-}

nixops/npm/node-packages.nix
@@ -1,26 +1,0 @@
-# This file has been generated by node2nix 1.6.0. Do not edit!
-
-{nodeEnv, fetchurl, fetchgit, globalBuildInputs ? []}:
-
-let
-  sources = {};
-in
-{
-  codemirror = nodeEnv.buildNodePackage {
-    name = "codemirror";
-    packageName = "codemirror";
-    version = "5.42.0";
-    src = fetchurl {
-      url = "https://registry.npmjs.org/codemirror/-/codemirror-5.42.0.tgz";
-      sha512 = "pbApC8zDzItP3HRphD6kQVwS976qB5Qi0hU3MZMixLk+AyugOW1RF+8XJEjeyl5yWsHNe88tDUxzeRh5AOxPRw==";
-    };
-    buildInputs = globalBuildInputs;
-    meta = {
-      description = "Full-featured in-browser code editor";
-      homepage = https://codemirror.net/;
-      license = "MIT";
-    };
-    production = true;
-    bypassCache = false;
-  };
-}

nixops/overlay.nix
@@ -1,4 +1,10 @@
 pkgsNew: pkgsOld: {
+  codemirror = pkgsNew.fetchzip {
+    url = "https://codemirror.net/5/codemirror.zip";
+
+    sha256 = "1dz1p789s9w5qlns86xfjkih88rhh930jz6v5j7azwgz4nqkcf41";
+  };
+
   docs =
     pkgsNew.runCommand "docs"
       { nativeBuildInputs = [
@@ -406,8 +412,6 @@ pkgsNew: pkgsOld: {
         sha256 = "0j7khvym2p4f8dqdl7k7k4rypj1hai2rj0pjzb6h41fvjyjmbrrr";
       };
   };
-
-  npm = pkgsNew.callPackage ./npm { };
 
   prelude-lint = pkgsNew.runCommand "prelude-lint" {} ''
     ${pkgsNew.rsync}/bin/rsync --archive ${pkgsNew.expected-prelude}/ ./Prelude.expected

nixops/website.nix
@@ -1,5 +1,5 @@
-{ coreutils, dhall-try, haskell, jQuery, logo, nodePackages, npm, runCommand
-, twitterBootstrap
+{ codemirror, coreutils, dhall-try, haskell, jQuery, logo, nodePackages
+, runCommand, twitterBootstrap
 }:
 
 runCommand "try-dhall" {} ''
@@ -13,12 +13,12 @@ runCommand "try-dhall" {} ''
   ${coreutils}/bin/ln --symbolic ${twitterBootstrap}/js/bootstrap.min.js $out/js
   ${coreutils}/bin/ln --symbolic ${twitterBootstrap}/js/bootstrap.min.js.map $out/js
   ${coreutils}/bin/ln --symbolic ${twitterBootstrap}/css/bootstrap.min.css $out/css
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/lib/codemirror.js $out/js
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/mode/haskell/haskell.js $out/js
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/mode/shell/shell.js $out/js
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/mode/javascript/javascript.js $out/js
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/mode/yaml/yaml.js $out/js
-  ${coreutils}/bin/ln --symbolic ${npm.codemirror}/lib/node_modules/codemirror/lib/codemirror.css $out/css
+  ${coreutils}/bin/ln --symbolic ${codemirror}/lib/codemirror.js $out/js
+  ${coreutils}/bin/ln --symbolic ${codemirror}/mode/haskell/haskell.js $out/js
+  ${coreutils}/bin/ln --symbolic ${codemirror}/mode/shell/shell.js $out/js
+  ${coreutils}/bin/ln --symbolic ${codemirror}/mode/javascript/javascript.js $out/js
+  ${coreutils}/bin/ln --symbolic ${codemirror}/mode/yaml/yaml.js $out/js
+  ${coreutils}/bin/ln --symbolic ${codemirror}/lib/codemirror.css $out/css
   ${coreutils}/bin/ln --symbolic ${dhall-try}/bin/dhall-try.jsexe/all.min.js $out/js
   ${coreutils}/bin/ln --symbolic ${logo.argocd} $out/img/argocd-logo.png
   ${coreutils}/bin/ln --symbolic ${logo.ansible} $out/img/ansible-logo.png

release.nix
@@ -36,7 +36,11 @@ let
   vm =
     (import "${nixpkgs}/nixos" {
       configuration = {
-        imports = [ ./nixops/logical.nix ./nixops/physical.nix ];
+        imports = [
+          ./nixops/logical.nix
+          ./nixops/physical.nix
+          "${nixpkgs}/nixos/modules/virtualisation/qemu-vm.nix"
+        ];
 
         networking.hostName = "dhall-lang";
 
@@ -51,7 +55,7 @@ let
         virtualisation = {
           cores = 2;
 
-          memorySize = "4096";
+          memorySize = 4096;
         };
       };
 

commit b992b2d903ab1f3a65f62be11740a90b8a937ad1

Fix logrotate configuration
This fixes breakage introduced by #1300 with respect to log
rotation.

The `services.logrotate.configFile` option is supposed to be
a file and not the contents of the file.   Wrapping the option
in `pkgs.writeText` fixes this.

nixops/logical.nix
@@ -149,7 +149,7 @@ in
     logrotate = {
       enable = true;
 
-      configFile = ''
+      configFile = pkgs.writeText "logrotate.conf" ''
         /var/spool/nginx/logs/*.log {
           create 0644 nginx nginx
           daily