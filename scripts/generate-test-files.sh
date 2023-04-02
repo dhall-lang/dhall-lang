43f08c6b431ada49c7c2df2b04d07b12c6c3bcc9
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2023-04-02 10:48:02 -0700

    Fix scripts on `zsh`

--- scripts/generate-test-files.sh
+++ scripts/generate-test-files.sh
@@ -1,13 +1,13 @@
 set -eu
 
-my-nix() {
+my_nix() {
     nix --extra-experimental-features 'nix-command flakes' "${@}"
 }
 
-my-rsync() {
-    my-nix run 'nixpkgs#rsync' -- "${@}"
+my_rsync() {
+    my_nix run 'nixpkgs#rsync' -- "${@}"
 }
 
-my-nix build --file ./release.nix expected-test-files
-my-rsync --archive --checksum --delete result/ ./tests
+my_nix build --file ./release.nix expected-test-files
+my_rsync --archive --checksum --delete result/ ./tests
 chmod -R u+w ./tests
