aad86798773ca1f1852610e8b0a1c019eb277eaf
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2026-03-21 12:11:35 -0700

    Mitigate spam for discourse.dhall-lang.org
    discourse.dhall-lang.org was recently subject to a spam attack.  I
    (hopefully) fixed this by changing the domain to be protected by
    Cloudflare, which also requires changing the host to reject incoming
    traffic that doesn't originate from Cloudflare.

--- nixops/logical.nix
+++ nixops/logical.nix
@@ -254,6 +254,31 @@ in
               proxy_set_header X-Forwarded-Host $host;
               proxy_set_header X-Forwarded-Server $host;
               proxy_set_header Accept-Encoding "";
+
+              # Require traffic to originate from Cloudflare to mitigate spam
+              allow 173.245.48.0/20;
+              allow 103.21.244.0/22;
+              allow 103.22.200.0/22;
+              allow 103.31.4.0/22;
+              allow 141.101.64.0/18;
+              allow 108.162.192.0/18;
+              allow 190.93.240.0/20;
+              allow 188.114.96.0/20;
+              allow 197.234.240.0/22;
+              allow 198.41.128.0/17;
+              allow 162.158.0.0/15;
+              allow 104.16.0.0/13;
+              allow 104.24.0.0/14;
+              allow 172.64.0.0/13;
+              allow 131.0.72.0/22;
+              allow 2400:cb00::/32;
+              allow 2606:4700::/32;
+              allow 2803:f800::/32;
+              allow 2405:b500::/32;
+              allow 2405:8100::/32;
+              allow 2a06:98c0::/29;
+              allow 2c0f:f248::/32;
+              deny all;
             '';
 
             enableACME = true;
